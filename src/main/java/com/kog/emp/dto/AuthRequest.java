661e6385b76c1bec46e5682cb93ae52971ba5de6
NandiniParnapalli <parnapallinandini2@gmail.com>
2024-04-26 12:00:51 +0530

    employee management commit2

--- src/main/java/com/kog/emp/dto/AuthRequest.java
+++ src/main/java/com/kog/emp/dto/AuthRequest.java
@@ -4,9 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class AuthRequest {
 
     private String username;
